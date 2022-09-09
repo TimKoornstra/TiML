b046a64f7aa32e49398ed9398cdb0218768e80d6
TimKoornstra <tim.koornstra@gmail.com>
2022-09-09 19:25:05 +0200

    Various bug fixes, additions
    Bug fixed: now include proportion of split in impurity measure DT
    Added: Docstrings for Decision Tree
    Added: Basic hyperparameters for Decision Tree
    
    To do: Allow different impurity functions for DT

--- models/trees.py
+++ models/trees.py
@@ -8,7 +8,24 @@ from losses.categorical import gini_impurity
 
 
 class DecisionTree():
-    def __init__(self):
+    """
+    A simple implementation of a Decision Tree
+
+    Attributes:
+    -----------
+    left : DecisionTree
+        A possible left child.
+    right : DecisionTree
+        A possible right child.
+    split_feature : int
+        The feature to split on in the case that this is a non-terminal node.
+    split_value : float
+        The value to split on in the case that this is a non-terminal node.
+    majority_class : int
+        The predicted class in the case that this is a leaf (terminal node).
+    """
+
+    def __init__(self, depth=1):
         # A tree can have left and right children
         self.left = None
         self.right = None
@@ -22,36 +39,74 @@ class DecisionTree():
         # The majority class (if this is a leaf node)
         self.majority_class = None
 
-    def fit(self, X, y, min_split=0, min_leaf=0):
+        # Store the depth of this node
+        self.depth = depth
 
-        # We need to determine the best feature and feature value combination to split on
-        # To do that, we need to calculate the impurity improvement for all combinations
+    def fit(self, X, y, min_split=2, max_depth=None):
+        """
+        Fit the decision tree on the data.
 
-        if X.shape[0] < min_leaf or X.shape[0] < min_split or np.max(y) == np.min(y):
+        Parameters:
+        -----------
+        X : numpy.ndarray
+            The input data of shape (m, n).
+        y : numpy.ndarray
+            The labels of shape (m, ).
+        min_split : int
+            The minimum number of samples required to split an internal node.
+        max_depth : int
+            The maximum depth of the Decision Tree.
+
+        Returns:
+        --------
+        self : object
+            Fitted Decision Tree.
+        """
+        # Determine if this is a leaf node
+
+        if (X.shape[0] < min_split or self.depth == max_depth
+                or np.max(y) == np.min(y)):
             self.majority_class = np.argmax(np.bincount(y))
             return self
 
-        current_impurity = gini_impurity(y)
-        lowest_impurity = float("-inf")
+        # We need to determine the best feature and feature value combination
+        # to split on. To do that, we need to calculate the impurity
+        # improvement for all combinations.
+        best_impurity = gini_impurity(y)
 
         for i in range(X.shape[1]):
-            # Determine all possible splits and their corresponding split values for this feature
+            # Determine all possible splits and their corresponding split
+            # values for this feature.
             feature_values_sorted = np.unique(X[:, i])
-            feature_possible_splits = np.unique((feature_values_sorted[:-1] + feature_values_sorted[1:]) / 2)
+            feature_possible_splits = np.unique(
+                (feature_values_sorted[:-1] + feature_values_sorted[1:]) / 2)
 
             for split in feature_possible_splits:
                 # Determine the left and right indices
-                # Left contains all values <= split, right all the values > split
+                # Left contains all values <= split, right the values > split
                 left_split_ind = np.where(X[:, i] <= split)[0]
                 right_split_ind = np.where(X[:, i] > split)[0]
 
                 # Calculate the impurity for this split
-                new_impurity = current_impurity - gini_impurity(y[left_split_ind]) + gini_impurity(y[right_split_ind])
-                # If this beats our old impurity, save it
-                if new_impurity > lowest_impurity:
-                    current_impurity = new_impurity
+                # The new impurity is calculated using:
+                # \pi(l) * impurity(l) + \pi(r) * impurity(r)
+                # Where \pi stands for "proportion"
+                proportion_l = len(left_split_ind) / len(y)
+                proportion_r = len(right_split_ind) / len(y)
+
+                new_impurity = proportion_l * gini_impurity(y[left_split_ind])\
+                    + proportion_r * gini_impurity(y[right_split_ind])
+
+                # If this beats our best impurity, save it
+                if new_impurity < best_impurity:
+                    best_impurity = new_impurity
                     self.split_feature = i
                     self.split_value = split
+
+        # If we have not found any improvement, consider this a leaf
+        if self.split_feature is None and self.split_value is None:
+            self.majority_class = np.argmax(np.bincount(y))
+            return self
 
         # Split our data based on the best split
         left_indices = np.where(X[:, self.split_feature] <= self.split_value)
@@ -61,16 +116,29 @@ class DecisionTree():
         right_X, right_y = X[right_indices], y[right_indices]
 
         # Create children
-        self.left = DecisionTree()
-        self.right = DecisionTree()
+        self.left = DecisionTree(depth=self.depth+1)
+        self.right = DecisionTree(depth=self.depth+1)
 
         # Grow the child nodes
-        self.left.fit(left_X, left_y)
-        self.right.fit(right_X, right_y)
+        self.left.fit(left_X, left_y, min_split, max_depth)
+        self.right.fit(right_X, right_y, min_split, max_depth)
 
         return self
 
     def predict(self, X, y):
+        """
+        Function to predict the output of the model.
+
+        Parameters:
+        -----------
+        X : numpy.ndarray
+            The input data of shape (m, n).
+
+        Returns:
+        --------
+        y_pred : numpy.ndarray
+            The predicted labels of shape (m,).
+        """
         y_pred = []
 
         # Make a prediction for each of our datapoints
@@ -78,13 +146,15 @@ class DecisionTree():
             current_node = self
 
             while True:
-                # If the current node has a majority class (i.e. it is a leaf), append it to the results
+                # If the current node has a majority class (i.e. it is a leaf),
+                # append it to the results.
                 if current_node.majority_class is not None:
                     y_pred.append(current_node.majority_class)
                     break
                 else:
                     # Otherwise keep going
-                    if datapoint[current_node.split_feature] <= current_node.split_value:
+                    if (datapoint[current_node.split_feature] <=
+                            current_node.split_value):
                         current_node = current_node.left
                     else:
                         current_node = current_node.right
